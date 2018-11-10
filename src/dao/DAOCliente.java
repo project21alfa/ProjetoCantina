d622d2d83a048314b30f6501b534f83372344ac7
JONATAS <JONATAS@JONATAS-PC>
2018-11-10 16:13:45 -0200

    - Finalização TelaConsultaCliente

--- src/dao/DAOCliente.java
+++ src/dao/DAOCliente.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceException;
 import javax.persistence.Query;
 
 public class DAOCliente {
@@ -25,5 +26,26 @@ public class DAOCliente {
         
         return listClientes;
         
+    }
+    
+    public boolean excluir(long id){
+        em = Fabrica.getFabrica().createEntityManager();
+        t = em.getTransaction();
+        
+        try{
+           t.begin();
+           Cliente c = em.find(new Cliente().getClass(), id);
+           em.remove(c);
+           t.commit();
+           return true;
+        }
+        catch(PersistenceException pe){
+            pe.printStackTrace();
+            t.rollback();
+            return false;
+        }
+        finally{
+            em.close();
+        }
     }
 }
--- src/tela/Consulta/TelaConsultaCliente.java
+++ src/tela/Consulta/TelaConsultaCliente.java
@@ -1,14 +1,18 @@
 package tela.Consulta;
 
+import dao.DAOCliente;
 import entidades.Cliente;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+import tela.alterar.TelaAlterarCliente;
 
 public class TelaConsultaCliente extends javax.swing.JInternalFrame {
-    private Cliente cliente;
+
+    private DAOCliente dao = new DAOCliente();
     private List<Cliente> listCliente = new ArrayList<>();
-    
+
     public TelaConsultaCliente() {
         initComponents();
     }
@@ -17,11 +21,30 @@ public class TelaConsultaCliente extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        popupMenu = new javax.swing.JPopupMenu();
+        miAlterar = new javax.swing.JMenuItem();
+        miExcluir = new javax.swing.JMenuItem();
         jScrollPane1 = new javax.swing.JScrollPane();
         tabela = new javax.swing.JTable();
         jScrollPane2 = new javax.swing.JScrollPane();
         txtNome = new javax.swing.JTextPane();
         jButton1 = new javax.swing.JButton();
+
+        miAlterar.setText("jMenuItem1");
+        miAlterar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                miAlterarActionPerformed(evt);
+            }
+        });
+        popupMenu.add(miAlterar);
+
+        miExcluir.setText("Excluir");
+        miExcluir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                miExcluirActionPerformed(evt);
+            }
+        });
+        popupMenu.add(miExcluir);
 
         tabela.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -66,50 +89,69 @@ public class TelaConsultaCliente extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void miAlterarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miAlterarActionPerformed
+        Cliente c = listCliente.get(tabela.getSelectedRow());
+        String senha = JOptionPane.showInputDialog("Informe a senha");
+        
+        TelaAlterarCliente telaAC = new TelaAlterarCliente(null, true, c);
+        telaAC.setVisible(true);
+    }//GEN-LAST:event_miAlterarActionPerformed
+
+    private void miExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miExcluirActionPerformed
+        if (tabela.getSelectedRow() != -1) {
+
+            int confirm = JOptionPane.showConfirmDialog(null, "Deseja realmente excluir?",
+                    "ATENÇÃO!!!", JOptionPane.YES_NO_OPTION);
+
+            if (confirm == JOptionPane.YES_OPTION) {
+                Cliente c = listCliente.get(tabela.getSelectedRow());
+                dao.excluir(c.getId());
+            }
+        }
+    }//GEN-LAST:event_miExcluirActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JMenuItem miAlterar;
+    private javax.swing.JMenuItem miExcluir;
+    private javax.swing.JPopupMenu popupMenu;
     private javax.swing.JTable tabela;
     private javax.swing.JTextPane txtNome;
     // End of variables declaration//GEN-END:variables
-    
-    private void updateTable(){
-        
-        DefaultTableModel modelo = new DefaultTableModel(){
+
+    private void updateTable() {
+
+        DefaultTableModel modelo = new DefaultTableModel() {
             @Override
             public boolean isCellEditable(int row, int column) {
                 return false;
             }
-            
+
         };
-     
+
         modelo.addColumn("Nome");
         modelo.addColumn("Cidade");
         modelo.addColumn("Estado");
         modelo.addColumn("Email");
         modelo.addColumn("Telefone");
         modelo.addColumn("CPF");
-        
-        for(Cliente c : listCliente){
-            
+
+        for (Cliente c : listCliente) {
+
             modelo.addRow(new Object[]{
                 c.getNome(),
                 c.getCidade().getNome(),
                 c.getCidade().getEstado().getNome(),
                 c.getEmail(),
                 c.getTelefone(),
-                c.getCpf()            
+                c.getCpf()
             });
-            
-        }       
-        
-        tabela.setModel(modelo);             
-    }
-    
-    public Cliente cli(){
 
-        return cliente;
-    }
+        }
+
+        tabela.setModel(modelo);
+    }    
 }
